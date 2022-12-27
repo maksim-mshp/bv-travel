c083010d8933022c65301b26095e8939119f2374
Максим Чернышков <maksim-mshp@yandex.ru>
2022-12-27 20:12:51 +0300

    главная картинка

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
+		<v-app-bar class="header" app color="primary" dark>
 			<div class="d-flex align-center">
-				<v-img
-					alt="Vuetify Logo"
-					class="shrink mr-2"
-					contain
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-					transition="scale-transition"
-					width="40"
-				/>
-
-				<v-img
-					alt="Vuetify Name"
-					class="shrink mt-1 hidden-sm-and-down"
-					contain
-					min-width="100"
-					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-					width="100"
-				/>
+				Название
 			</div>
 
 			<v-spacer></v-spacer>
 
 			<v-btn
-				href="https://github.com/vuetifyjs/vuetify/releases/latest"
+				to="/create"
 				target="_blank"
 				text
 			>
-				<span class="mr-2">Latest Release</span>
-				<v-icon>mdi-open-in-new</v-icon>
+				<span>Создать маршрут</span>
+			</v-btn>
+            <v-btn
+				to="/popular"
+				target="_blank"
+				text
+			>
+				<span>Популярные маршруты</span>
+			</v-btn>
+            <v-btn
+				to="/create"
+				target="_blank"
+				text
+			>
+				<span>Интересные места</span>
 			</v-btn>
 		</v-app-bar>
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,48 @@
 <template>
-  <p>Main</p>
+	<div class="main-wrap">
+		<v-img
+			src="../assets/tabea-schimpf-9-xfYKAI6ZI-unsplash.jpg"
+			class="main-img"
+		></v-img>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: 'Main'
-  }
+export default {
+	name: "Main",
+	data: () => ({
+		height: 600,
+	}),
+	computed: {
+		getHeight() {
+			return 600;
+		},
+	},
+	methods: {
+		resize() {
+            let x = document.querySelector('.header').getBoundingClientRect().height;
+			document.querySelector('.main-img').style['height'] = `calc(100vh - ${x}px)`
+		},
+	},
+	mounted() {
+        this.resize();
+		window.addEventListener(
+			"resize",
+			function (event) {
+				this.resize();
+			},
+			true
+		);
+	},
+};
 </script>
+
+<style scoped>
+.main-wrap {
+	height: 100%;
+}
+
+.main-img {
+    height: 600px;
+}
+</style>
