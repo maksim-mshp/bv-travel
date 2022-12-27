6c0783bfd67c5fc968cfc7fb07793c5a21ef0657
Максим Чернышков <maksim-mshp@yandex.ru>
2022-12-27 12:50:47 +0300

    добавлен axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import "./plugins/axios";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.axios = axios;
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app");
