222a81e5fd9890f3073c1326c24f53d7c0038fc5
Максим Чернышков <maksim-mshp@yandex.ru>
2023-02-01 17:47:33 +0300

    logo and favicons

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
     <v-app>
         <v-app-bar class="header" app color="primary" dark>
-            <router-link to="/">
-                <v-toolbar-title>Название</v-toolbar-title></router-link
-            >
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+
+            <router-link to="/" class="logo-link">
+                <img src="@/assets/white_logo.svg">
+            </router-link>
 
             <v-spacer></v-spacer>
 
-            <router-link to="/create">
+            <router-link to="">
                 <v-btn text>
-                    <span>Создать маршрут</span>
+                    <span> Войти </span>
                 </v-btn>
             </router-link>
-            <router-link to="/popular">
-            <v-btn text>
-                <span>Популярные маршруты</span>
-            </v-btn>
-            </router-link>
-            <router-link to="/places">
-            <v-btn text>
-                <span>Интересные места</span>
-            </v-btn>
-            </router-link>
         </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary class="navigation-drawer">
+            <v-list nav dense>
+                <v-list-item-group
+                    v-model="group"
+                    color="primary"
+                >
+                    <router-link to="/"><v-list-item>
+                        <v-list-item-title>Главная</v-list-item-title>
+                    </v-list-item></router-link>
+                    
+                    <router-link to="/create"><v-list-item>
+                        <v-list-item-title>Создать маршрут</v-list-item-title>
+                    </v-list-item></router-link>
+
+                    <router-link to="/popular"><v-list-item>
+                        <v-list-item-title>Популярные маршруты</v-list-item-title>
+                    </v-list-item></router-link>
+
+                    <router-link to="/places"><v-list-item>
+                        <v-list-item-title>Интересные места</v-list-item-title>
+                    </v-list-item></router-link>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
 
         <v-main>
             <router-view />
@@ -41,15 +60,42 @@ export default {
     },
 
     data: () => ({
-        //
+        drawer: false,
+        group: null,
     }),
 
+    watch: {
+        group() {
+            this.drawer = false;
+        },
+    },
+
     mounted() {
-        this.axios.defaults.headers.common['X-Master-Key'] = '$2b$10$tf15G4xzYpMvghS3gZ5q4ug.LaMxTEgt/kSgag4gKYezwhz0Jxr0y';
-		this.axios.defaults.headers.common['Content-Type'] = 'application/json';
-    }
+        this.axios.defaults.headers.common["X-Master-Key"] =
+            "$2b$10$tf15G4xzYpMvghS3gZ5q4ug.LaMxTEgt/kSgag4gKYezwhz0Jxr0y";
+        this.axios.defaults.headers.common["Content-Type"] = "application/json";
+    },
 };
 </script>
+
+<style scoped>
+a {
+    text-decoration: none !important;
+}
+
+.logo-link {
+    display: flex;
+}
+
+.logo-link > img {
+    height: 28px;
+    margin-left: 10px;
+}
+
+.navigation-drawer {
+    font-weight: 600;
+}
+</style>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap");
@@ -58,12 +104,5 @@ body,
 h1,
 #app {
     font-family: "Montserrat", sans-serif !important;
-}
-</style>
-
-<style scoped>
-a {
-    text-decoration: none !important;
-    color: white !important;
 }
 </style>
